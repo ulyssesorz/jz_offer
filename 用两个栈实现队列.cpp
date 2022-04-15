5e2f0e362eefffdec2bcb35e38744b1134b8f700
ulyssesorz <2284903150@qq.com>
2022-04-15 11:37:37 +0800

    master

--- 用两个栈实现队列.cpp
+++ 用两个栈实现队列.cpp
@@ -18,18 +18,18 @@ public:
     
     int deleteHead() 
     {
-        if(stack_2.empty())
+        if(stack_2.empty()) 
         {
             if(stack_1.empty())
                 return -1;
             while(!stack_1.empty())
             {
-                int temp=stack_1.top();
+                int temp=stack_1.top(); //stack_1元素全部移动到stack_2
                 stack_1.pop();
                 stack_2.push(temp);
             }
         }
-        int num=stack_2.top();
+        int num=stack_2.top();  //stack_2非空时直接输出
         stack_2.pop();
         return num;
     }
