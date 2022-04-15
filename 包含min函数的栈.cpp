2df72f9318350b9d74d9a1fd42c37b49194eb0e5
ulyssesorz <2284903150@qq.com>
2022-04-15 17:40:42 +0800

    2

--- 包含min函数的栈.cpp
+++ 包含min函数的栈.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
 class MinStack 
 {
 private:
@@ -10,7 +14,9 @@ public:
     void push(int x) 
     {
         st.push(x);
-        if(x<st_min.top())
+        if(st_min.empty())  
+            st_min.push(x);
+        else if(x<=st_min.top())    //x（也）是最小值，入min栈保存
             st_min.push(x);
     }
     
@@ -18,7 +24,7 @@ public:
     {
         int temp=st.top();
         st.pop();
-        if(temp==st_min.top())
+        if(!st_min.empty()&&temp==st_min.top()) //st出的是最小值，则st_min也弹出
             st_min.pop();
     }
     
@@ -32,3 +38,15 @@ public:
         return st_min.top();
     }
 };
+
+int main()
+{   
+    MinStack *minStack = new MinStack();
+    minStack->push(0);
+    minStack->push(1);
+    minStack->push(0);
+    cout<<minStack->min()<<endl;   
+    minStack->pop();    
+    cout<<minStack->min()<<endl;   
+    return 0;
+}
