13833d608a580ae30054e490b1e562ac143b577d
ulyssesorz <2284903150@qq.com>
2022-04-15 11:04:40 +0800

    1

---
+++ 用两个栈实现队列.cpp
@@ -0,0 +1,36 @@
+#include<cstdio>
+#include<stack>
+using namespace std;
+
+
+class CQueue 
+{
+public:
+    stack<int> stack_1,stack_2;
+    CQueue() 
+    {
+    }
+    
+    void appendTail(int value) 
+    {
+        stack_1.push(value);
+    }
+    
+    int deleteHead() 
+    {
+        if(stack_2.empty())
+        {
+            if(stack_1.empty())
+                return -1;
+            while(!stack_1.empty())
+            {
+                int temp=stack_1.top();
+                stack_1.pop();
+                stack_2.push(temp);
+            }
+        }
+        int num=stack_2.top();
+        stack_2.pop();
+        return num;
+    }
+};
