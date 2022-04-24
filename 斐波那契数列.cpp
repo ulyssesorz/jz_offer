2dd9929bd4fe4527710b9926ca44a38169a4c4a9
ulyssesorz <2284903150@qq.com>
2022-04-24 11:51:27 +0800

    20

--- 斐波那契数列.cpp
+++ 斐波那契数列.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int fib(int n) {
         if(n==0)
-            return 0;   //排除0的特殊情况
+            return 0;
+        if(n==1)
+            return 1;   //前两项0,1的结果
         int i=0,j=1;
         int temp;
-        while(--n){
+        for(int k=2;k<=n;k++){      //后n-2项的结果
             temp=j;
-            j=(i+j)%1000000007; 
-            i=temp;     //向后滑动
+            j=(i+j)%1000000007;
+            i=temp;
         }
         return j;
     }
