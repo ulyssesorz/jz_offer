4ed2ff5e3ce082f48df0349653b3eb40cb3102df
ulyssesorz <2284903150@qq.com>
2022-04-26 19:31:15 +0800

    25

--- 把数字翻译成字符串.cpp
+++ 把数字翻译成字符串.cpp
@@ -5,7 +5,7 @@ public:
         int i=1,j=1,temp;
         for(int k=2;k<=str.size();++k){
             if(str.substr(k-2,2)>="10" && str.substr(k-2,2)<="25")
-                temp=i+j;
+                temp=i+j;   //满足条件则滚动
             else
                 temp=j;
             i=j;
@@ -20,10 +20,10 @@ public:
     int translateNum(int num) {
         if(num<10)
             return 1;
-        int m=num%100;
-        if(m<10 || m>25)
-            return translateNum(num/10);    //只能转为一个数
+        int m=num%100;  //取两位数
+        if(m>=10 && m<=25)
+            return translateNum(num/10)+translateNum(num/100);  //能翻译两位
         else
-            return translateNum(num/10)+translateNum(num/100);  //可以转为两个数
+            return translateNum(num/10);    //只能翻译一位
     }
 };
