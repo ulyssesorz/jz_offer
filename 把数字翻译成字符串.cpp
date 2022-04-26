6cbb04b68f643c74196a1a5dd78b83b707fceab2
ulyssesorz <2284903150@qq.com>
2022-04-26 19:30:56 +0800

    25

---
+++ 把数字翻译成字符串.cpp
@@ -0,0 +1,29 @@
+class Solution {  //动态规划
+public:
+    int translateNum(int num) {
+        string str=to_string(num);
+        int i=1,j=1,temp;
+        for(int k=2;k<=str.size();++k){
+            if(str.substr(k-2,2)>="10" && str.substr(k-2,2)<="25")
+                temp=i+j;
+            else
+                temp=j;
+            i=j;
+            j=temp;
+        }
+        return j;
+    }
+};
+
+class Solution { //递归
+public:
+    int translateNum(int num) {
+        if(num<10)
+            return 1;
+        int m=num%100;
+        if(m<10 || m>25)
+            return translateNum(num/10);    //只能转为一个数
+        else
+            return translateNum(num/10)+translateNum(num/100);  //可以转为两个数
+    }
+};
