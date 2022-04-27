42e8ae6a5f775a40467cd49308bf4762f6329be5
ulyssesorz <2284903150@qq.com>
2022-04-27 17:24:16 +0800

    33

---
+++ 翻转单词顺序.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res="";
+        for(int i=s.size()-1;i>=0;--i){
+            if(s[i]!=' '){
+                int j=i;
+                while(i>=0 && s[i]!=' ')
+                    --i;
+                res+=substr(i+1,j-i);
+            }
+        }
+        return res;
+    }
+};
