7c955a7cd6d1e42aec05a84b82db38503e229034
ulyssesorz <2284903150@qq.com>
2022-05-06 20:33:28 +0800

    49

---
+++ 数值的整数次方.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x == 0)
+            return 0;
+        long N = n; //防溢出
+        double res = 1;
+        if(N < 0){
+            x = 1 / x;
+            N = -N;
+        }
+        while(N > 0){
+            if(N & 1 == 1)  
+                res *= x;
+            x *= x;
+            N >>= 1;    //后移一位
+        }
+        return res;
+    }
+};
