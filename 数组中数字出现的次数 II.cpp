ba7021f68ad37ff83b126d2c4f8eaa55519f1f01
ulyssesorz <2284903150@qq.com>
2022-05-07 19:53:38 +0800

    54

---
+++ 数组中数字出现的次数 II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        vector<int> vec(32);
+        for(int n : nums){
+            for(int i = 31; i >= 0; --i){   //统计每一位非0的个数
+                vec[i] += n & 1;    
+                n >>= 1;
+            }
+        }
+        for(int i = 0; i < 32; ++i){
+            if(vec[i] % 3 != 0)         //不被3整除说明这一位是唯一的那个数的位
+                res += pow(2,31 - i);
+        }
+        return res;
+    }
+};
