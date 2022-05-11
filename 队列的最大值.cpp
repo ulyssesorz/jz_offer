73ca789b490e5f1d3a405dc082bf05bfb96aa6f6
ulyssesorz <2284903150@qq.com>
2022-05-11 19:05:45 +0800

    65

---
+++ 队列的最大值.cpp
@@ -0,0 +1,32 @@
+class MaxQueue {
+public:
+    MaxQueue() {
+
+    }
+    
+    int max_value() {
+        if(!dq.empty())
+            return dq[0];
+        return -1;
+    }
+    
+    void push_back(int value) {
+        q.push(value);
+        while(!dq.empty() && dq.back() <= value)    //保持单调性
+            dq.pop_back();
+        dq.push_back(value);
+    }
+    
+    int pop_front() {
+        if(q.empty())
+            return -1;
+        int top = q.front();
+        q.pop();
+        if(!dq.empty() && top == dq.front())
+            dq.pop_front();
+        return top;
+    }
+private:
+    queue<int> q;
+    deque<int> dq;
+};
